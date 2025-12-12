0d9401ea9355e104f309a718655c4c67948f3cfe
Kseniia Shinkar <k3361603@gmail.com>
2025-12-12 18:32:35 +0300

    delivery details commit

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from menu.views import index
+from menu.views import index, order_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +10,7 @@ urlpatterns = [
     # Все адреса из приложения menu будут начинаться с /api/
     path('api/', include('menu.urls')),
     path('', index, name='index'),
+    path('order/<int:pk>/', order_detail, name='order_detail'),
 ]
 
 if settings.DEBUG:
--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from .models import Product, Category, Order
 from .serializers import ProductSerializer, CategorySerializer, OrderCreateSerializer
-from django.shortcuts import render
+
 from rest_framework.permissions import AllowAny
+from django.shortcuts import render, get_object_or_404
 
 # Ручка 1: Получить список всех товаров
 class ProductListView(generics.ListAPIView):
@@ -20,6 +21,13 @@ class CreateOrderView(generics.CreateAPIView):
     permission_classes = [AllowAny]  # Разрешить всем (даже анонимам)
     authentication_classes = []
 
+# HTML: Главная
 def index(request):
     products = Product.objects.filter(is_available=True)
     return render(request, 'index.html', {'products': products})
+
+# HTML: Страница заказа
+def order_detail(request, pk):
+    # Ищем заказ по ID (pk), если нет - 404 ошибка
+    order = get_object_or_404(Order, pk=pk)
+    return render(request, 'order_detail.html', {'order': order})
